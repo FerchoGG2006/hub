d422da7a70d1790077bf443b7748c0457c9a5079
FerchoGG2006 <fernandobaquero45@gmail.com>
2026-04-24 22:44:11 -0500

    feat: implement CartContext provider for state management of cart items and customizations

--- src/CartContext.jsx
+++ src/CartContext.jsx
@@ -1,5 +1,6 @@
 import React, { createContext, useState, useCallback } from 'react';
 
+// eslint-disable-next-line react-refresh/only-export-components
 export const CartContext = createContext();
 
 export const CartProvider = ({ children }) => {
